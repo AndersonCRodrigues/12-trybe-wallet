51742d8eeed11725c2280f4489d6d47381994f19
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-20 15:05:58 -0300

    add base da tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import '../styles/Table.css';
 
 class Table extends Component {
+  state = {
+    linha: ['Descrição', 'Tag', 'Método de pagamento',
+      'Valor', 'Moeda', 'Câmbio utilizado', 'Valor convertido',
+      'Moeda de conversão', 'Editar/Excluir'],
+  };
+
   render() {
+    const { linha } = this.state;
     return (
-      <div>Table</div>
+      <tr>
+        {linha.map((item) => <th key={ item }>{item}</th>)}
+      </tr>
     );
   }
 }
