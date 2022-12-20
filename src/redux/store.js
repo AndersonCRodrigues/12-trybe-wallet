dd74400af2d9d04dc330a91598219995986536bd
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-20 09:19:36 -0300

    add config base do redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,18 @@
 // configure aqui sua store
+import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import userReducer from './reducers/user';
+import walletReducer from './reducers/wallet';
+
+const rootRedcucer = combineReducers({
+  user: userReducer,
+  wallet: walletReducer,
+});
+
+const store = createStore(rootRedcucer, composeWithDevTools());
+
+export default store;
+
+if (window.Cypress) {
+  window.store = store;
+}
