fe769cc00059743cf926d07f8e10b895a5d0dd28
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-20 15:30:27 -0300

    add despesas a tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,7 @@
+import P from 'prop-types';
 import React, { Component } from 'react';
 import '../styles/Table.css';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   state = {
@@ -10,12 +12,43 @@ class Table extends Component {
 
   render() {
     const { linha } = this.state;
+    const { expenses } = this.props;
     return (
-      <tr>
-        {linha.map((item) => <th key={ item }>{item}</th>)}
-      </tr>
+      <table>
+        <thead>
+          <tr>
+            {linha.map((item) => <th key={ item }>{item}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.length > 0 && expenses.map((item) => (
+            <tr key={ item.id }>
+              <td>{item.description}</td>
+              <td>{item.tag}</td>
+              <td>{item.method}</td>
+              <td>{(+item.value).toFixed(2)}</td>
+              <td>{item.exchangeRates[item.currency].name}</td>
+              <td>{`R$ ${(+item.exchangeRates[item.currency].ask).toFixed(2)}`}</td>
+              <td>
+                {`R$ ${
+                  (+item.value * item.exchangeRates[item.currency].ask).toFixed(2)}`}
+
+              </td>
+              <td>Real</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: P.arrayOf(P.shape(P.string)).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
